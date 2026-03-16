d7151b62663f7218c0dbf9e409bb5d740d4a6bea
Артём <komarovoleg512@gmail.com>
2026-03-16 13:02:25 +0700

    Delete ToDoList/djan directory

--- ToDoList/djan/djan/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from hello import views
-
-urlpatterns = [
-    path('prime/', views.prime),
-    path('Task/', views.Task),
-    path('CreateTask/', views.CreateTask),
-    path('delete/<int:id>', views.delete),
-]
--- ToDoList/djan/hello/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Tasks(models.Model):
-    Name = models.CharField(max_length=50)
-    Task = models.CharField(max_length=150)
-    Time = models.TimeField()
