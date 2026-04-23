e4d35052c1c5e59cbf3d597fd18bcd4dbd66bdaf
Артём <komarovoleg512@gmail.com>
2026-04-23 21:04:22 +0700

    Delete Chat directory

--- Chat/ai/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from hello import views
-
-urlpatterns = [
-    path('chat/', views.chat),
-    path('chat/error/', views.error),
-]
--- Chat/hello/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Searchs(models.Model):
-    Search = models.CharField()
-    Url = models.CharField()
