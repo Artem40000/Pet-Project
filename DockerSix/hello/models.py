b3d2ff8dda072d5e5945240562811481dc8562c2
Артём <komarovoleg512@gmail.com>
2026-04-23 21:04:31 +0700

    Delete DockerSix directory

--- DockerSix/hello/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Person(models.Model):
-    Name = models.CharField(max_length=16)
-    Age = models.IntegerField()
-    Password = models.CharField(max_length=24)
